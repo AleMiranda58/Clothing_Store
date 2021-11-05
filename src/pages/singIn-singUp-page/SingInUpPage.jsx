07bb778c09807c972b286fff2167edd535a2a9c8
Alejandra Miranda <ale.miranda890@gmail.com>
2021-11-05 15:36:57 -0400

    style: SignIn and SignInUp page

--- src/pages/singIn-singUp-page/SingInUpPage.jsx
+++ src/pages/singIn-singUp-page/SingInUpPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SignIn from '../../components/signIn/SignIn'
+import SignUp from '../../components/singUp/SignUp'
 import './SingInUpPage.scss'
 
 const SingInUpPage = () => {
     return (
-        <div>
+        <div className='sign-in-and-sign-up'>
             <SignIn />
+            <SignUp />
         </div>
     )
 }
