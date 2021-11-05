4c597e0403436e8f26c2d5a280144b2bdf4a414d
Alejandra Miranda <ale.miranda890@gmail.com>
2021-11-05 12:08:55 -0400

    feat: firebase authentication of users signing in/out with google accounts

--- src/App.js
+++ src/App.js
@@ -5,19 +5,40 @@ import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shoppage/ShopPage';
 import Header from './components/header/Header';
 import SingInUpPage from './pages/singIn-singUp-page/SingInUpPage';
+import { auth } from './firebase/firebase.utils'
 import './App.css'
 
-function App() {
-  return (
-    <div>
-    <Header />
-    <Switch>
-    <Route exact path='/' component={HomePage}/>
-    <Route path='/shop' component={ShopPage}/>
-    <Route path='/signin' component={SingInUpPage}/>
-    </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+  unsubcribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubcribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+    })
+  }
+  componentWillUnmount() {
+    this.unsubcribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+      <Header currentUser={this.state.currentUser} />
+      <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/shop' component={ShopPage}/>
+      <Route path='/signin' component={SingInUpPage}/>
+      </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
