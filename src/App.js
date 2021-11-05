f19c9add595e6c2c8c011a195b393b7de500528f
Alejandra Miranda <ale.miranda890@gmail.com>
2021-11-05 14:35:54 -0400

    feat: Store data from our DB/FireStore into our App -> STATE

--- src/App.js
+++ src/App.js
@@ -19,8 +19,23 @@ class App extends React.Component {
   unsubcribeFromAuth = null
 
   componentDidMount() {
-    this.unsubcribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user)
+    this.unsubcribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          },()=> {
+            console.log(this.state);
+          })
+        });
+      }
+      else {
+        this.setState({currentUser: userAuth})
+      }
     })
   }
   componentWillUnmount() {
