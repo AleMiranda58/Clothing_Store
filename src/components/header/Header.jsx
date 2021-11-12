dd7824a2d191505983c149be237c91cfa25ea56d
Alejandra Miranda <ale.miranda890@gmail.com>
2021-11-12 14:25:10 -0500

    feat: conecting user.reducer value into header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { auth } from '../../firebase/firebase.utils'
+import { connect } from 'react-redux'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.scss'
 
@@ -25,7 +26,10 @@ const Header = ({ currentUser }) => {
                 }
             </div>
         </div>
-    )
+    )   
 }
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
 
-export default Header
+export default connect(mapStateToProps)(Header)
